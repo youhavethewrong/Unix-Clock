ed3d6c0f54343a91f57d774127d6c4396c2cca27
Erick S. Crager <negated@oubliette.youhavethewrong.info>
2012-01-17 20:27:32 -0600

    initial commit

---
+++ src/info/youhavethewrong/unixclock/UnixClockActivity.java
@@ -0,0 +1,13 @@
+package info.youhavethewrong.unixclock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UnixClockActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
