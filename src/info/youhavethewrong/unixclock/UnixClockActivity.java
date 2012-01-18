848b2760053545055803468e6a2ac3f5d1ef9df5
Erick S. Crager <negated@oubliette.youhavethewrong.info>
2012-01-17 22:35:32 -0600

    Updating the clock on a lot of lifecycle actions.  It's stupid but kinda fun.  Obviously not a final solution.

--- src/info/youhavethewrong/unixclock/UnixClockActivity.java
+++ src/info/youhavethewrong/unixclock/UnixClockActivity.java
@@ -6,6 +6,32 @@ import android.util.Log;
 import android.widget.EditText;
 
 public class UnixClockActivity extends Activity {
+	
+	protected void onPause() {
+		super.onPause();
+		updateTime();
+	}
+
+	protected void onRestart() {
+		super.onRestart();
+		updateTime();
+	}
+
+	protected void onResume() {
+		super.onResume();
+		updateTime();
+	}
+
+	protected void onStart() {
+		super.onStart();
+		updateTime();
+	}
+
+	protected void onStop() {
+		super.onStop();
+		updateTime();
+	}
+
 	static final String TAG = "UnixClockActivity";
 	
     public void onCreate(Bundle savedInstanceState) {
