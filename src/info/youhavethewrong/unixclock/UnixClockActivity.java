0401e19a62544e34094c318029f38667721109d9
Erick S. Crager <negated@oubliette.youhavethewrong.info>
2012-01-17 21:10:45 -0600

    Added git ignore.  Successfully found a libcall that gives me UnixTime.

--- src/info/youhavethewrong/unixclock/UnixClockActivity.java
+++ src/info/youhavethewrong/unixclock/UnixClockActivity.java
@@ -2,12 +2,18 @@ package info.youhavethewrong.unixclock;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.lang.System;
 
 public class UnixClockActivity extends Activity {
+	static final String TAG = "UnixClock";
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Log.d(TAG, "~~~~~~~~~~~~~~~~~"+System.currentTimeMillis()+"||||||||||");
     }
 }
