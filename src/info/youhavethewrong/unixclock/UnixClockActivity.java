994d427cde8c451541f86d999f34c54daa9c8ab3
Erick S. Crager <negated@oubliette.youhavethewrong.info>
2012-01-18 21:17:04 -0600

    Added new font for the clock.  Implemented color cycling.

--- src/info/youhavethewrong/unixclock/UnixClockActivity.java
+++ src/info/youhavethewrong/unixclock/UnixClockActivity.java
@@ -12,9 +12,13 @@ package info.youhavethewrong.unixclock;
  */
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
@@ -45,23 +49,25 @@ public class UnixClockActivity extends Activity implements Runnable {
 		};
 	};
 	
-	// Our little clock widget
+	// handles for parts of our ui
 	TextView et;
+	TextView ex;
+	LinearLayout ll;
 	
-	// no longer visible to the user.  will be restarted or destroyed.
-	protected void onStop() {
-		super.onStop();
-		// stop running our thread?
-	}
-
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
     	
     	Log.d(TAG, "onCreate()");
         setContentView(R.layout.main);
         
-        // grab a handle on the EditText widget
+        // grab a handle for the LinearLayout
+        ll = (LinearLayout)findViewById(R.id.clockLayout);
+        // grab a handle on the clock TextView widget
         et = (TextView)findViewById(R.id.unixTime);
+        ex = (TextView)findViewById(R.id.explainId);
+        
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Glass_TTY_VT220.ttf");
+        et.setTypeface(tf);
         
         // if our thread doesn't exist yet, create it and get it spinning
         if (runner == null) {
@@ -70,18 +76,46 @@ public class UnixClockActivity extends Activity implements Runnable {
         }
     }
     
-    public void updateTime() {
-    	// just want the seconds, not the millis
-        String utime = ""+(System.currentTimeMillis() / 1000l);
-        
-        // write out our new time to the EditText
-        et.setText(utime);    	
-    }
-
+	// called only once first time menu is clicked on
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu, menu);
+		return true;
+	}
+	
+	// called every time user clicks on menu
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		switch (item.getItemId()) {
+		case R.id.colorScheme:
+			this.cycleColorScheme();
+			Log.d(TAG, "colorScheme menu item");
+			break;
+		case R.id.clockFont:
+			// call font picker
+			Log.d(TAG, "clockFont menu item");
+			break;
+		case R.id.aboutUnixClock:
+			// display about splash
+			Log.d(TAG, "aboutUnixClock menu item");
+			break;
+		}
+		return true;
+	}
+	
+	// no longer visible to the user.  will be restarted or destroyed.
+	protected void onStop() {
+		super.onStop();
+		// stop running our thread?
+	}
+	
 	public void run() {
 		while (runner != null) {
 			try {
-				Thread.sleep(1000);
+				/**
+				 * want about 4 refreshes / second to get a fairly accurate time,
+				 * while keeping the amount of work reasonable
+				 */
+				Thread.sleep(250);
 				Log.i(TAG+".run()", "Tick");
 			}
 			catch (InterruptedException e) { 
@@ -89,6 +123,33 @@ public class UnixClockActivity extends Activity implements Runnable {
 			}
 			timeHandler.post(update_ui);
 		}
-		
 	}
+	
+    public void updateTime() {
+    	// just want the seconds, not the millis
+        String utime = ""+(System.currentTimeMillis() / 1000l);
+        
+        // write out our new time to the EditText
+        et.setText(utime);    	
+    }
+    
+    public void cycleColorScheme() {
+ 	   	// set text & background color
+    	int curText = et.getCurrentTextColor();
+    	int amber = ll.getResources().getColor(R.color.amber);
+    	int green = ll.getResources().getColor(R.color.green);
+    	int black = ll.getResources().getColor(R.color.black);
+    	int grey = ll.getResources().getColor(R.color.grey);
+    	
+    	if(curText == amber) {
+    		et.setTextColor(green);
+    		ex.setTextColor(green);
+    		ll.setBackgroundColor(black);
+    	}
+    	else {
+    		et.setTextColor(amber);
+    		ex.setTextColor(amber);
+    		ll.setBackgroundColor(grey);
+    	}
+    }
 }
