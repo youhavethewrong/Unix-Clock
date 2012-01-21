46a10c1834c1aad095fa4f9cb988c2f0597549ad
Erick S. Crager <negated@oubliette.youhavethewrong.info>
2012-01-20 20:19:21 -0600

    had to increase API level to 11 for now.  Will pull it back if I can figure out a way.  Also sketched out WidgetProvider and Service.

--- src/info/youhavethewrong/unixclock/UnixClockWidgetProvider.java
+++ src/info/youhavethewrong/unixclock/UnixClockWidgetProvider.java
@@ -1,5 +1,41 @@
 package info.youhavethewrong.unixclock;
 
-public class UnixClockWidgetProvider {
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
 
+public class UnixClockWidgetProvider extends AppWidgetProvider {
+	static final String TAG = "UnixClockWidgetProvider";
+
+	@Override
+	public void onDeleted(Context context, int[] appWidgetIds) {
+		// TODO Auto-generated method stub
+		super.onDeleted(context, appWidgetIds);
+	}
+
+	@Override
+	public void onDisabled(Context context) {
+		// TODO Auto-generated method stub
+		super.onDisabled(context);
+	}
+
+	@Override
+	public void onEnabled(Context context) {
+		// TODO Auto-generated method stub
+		super.onEnabled(context);
+	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		super.onReceive(context, intent);
+	}
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		// TODO Auto-generated method stub
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+	}
 }
--- src/info/youhavethewrong/unixclock/UnixClockWidgetService.java
+++ src/info/youhavethewrong/unixclock/UnixClockWidgetService.java
@@ -1,5 +1,78 @@
 package info.youhavethewrong.unixclock;
 
-public class UnixClockWidgetService {
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+import android.widget.RemoteViewsService.RemoteViewsFactory;
 
+public class UnixClockWidgetService extends RemoteViewsService {
+
+	@Override
+	public RemoteViewsFactory onGetViewFactory(Intent intent) {
+		return new UnixClockRemoteViewsFactory(this.getApplicationContext(), intent);
+	}
+
+}
+
+class UnixClockRemoteViewsFactory implements RemoteViewsFactory {
+
+	public UnixClockRemoteViewsFactory(Context applicationContext, Intent intent) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public RemoteViews getLoadingView() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RemoteViews getViewAt(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getViewTypeCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean hasStableIds() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDataSetChanged() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
